dc85acfc288e2ab0491e74e4158b9be960d2ec3f
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2018-12-01 10:27:13 -0500

    Added the timer

---
+++ Projects/Project-LDR/Assets/_Scripts/CoroutineManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CoroutineManager {
+
+	
+    public static void BeginCoroutine(IEnumerator routine, Coroutine container, MonoBehaviour parentBehaviour)
+    {
+        HaltCoroutine(container, parentBehaviour);
+
+        container = parentBehaviour.StartCoroutine(routine);
+    }
+
+    public static void HaltCoroutine(Coroutine container, MonoBehaviour parentBehaviour)
+    {
+        if(container != null)
+        {
+            parentBehaviour.StopCoroutine(container);
+            container = null;
+        }
+    }
+
+    public static IEnumerator EnableAfterDelay(GameObject obj, float delay, Coroutine container)
+    {
+        yield return new WaitForSeconds(delay);
+        obj.SetActive(true);
+        container = null;
+    }
+
+    public static IEnumerator EnableAfterDelay(MonoBehaviour behaviour, float delay, Coroutine container)
+    {
+        yield return new WaitForSeconds(delay);
+        behaviour.enabled = true;
+        container = null;
+    }
+
+    public static IEnumerator ShrinkScaleFrom(Transform trans, Vector3 initialScale, Vector3 targetScale, float shrinkTime, Coroutine container)
+    {
+        trans.localScale = initialScale;
+        float elapsedTime = 0;
+        while(elapsedTime < shrinkTime)
+        {
+            elapsedTime += Time.deltaTime;
+            trans.localScale = Vector3.Lerp(initialScale, targetScale, elapsedTime / shrinkTime);
+            yield return null;
+        }
+        trans.localScale = targetScale;
+        container = null;
+    }
+
+    public static IEnumerator FadeAlphaTo(CanvasGroup cg, float initialAlpha, float targetAlpha, float fadeTime, bool disableWhenDone, Coroutine container)
+    {
+        cg.alpha = initialAlpha;
+        float elapsedTime = 0;
+
+        while(elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            cg.alpha = Mathf.Lerp(initialAlpha, targetAlpha, elapsedTime / fadeTime);
+            yield return null;
+        }
+        cg.alpha = targetAlpha;
+
+        if (disableWhenDone)
+        {
+            cg.gameObject.SetActive(false);
+        }
+
+        container = null;
+    }
+}
---
+++ Projects/Project-LDR/Assets/_Scripts/Timer.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    [SerializeField, Tooltip("The image that will represent the time remaining.")] private Image timerGraphic;
+    [SerializeField, Tooltip("The transform of the text of the timer.")] private Transform timerTextTransform;
+    [SerializeField, Tooltip("The text that will display the time remaining.")] private Text timerText;
+
+    //Container for the text scaling coroutine.
+    private Coroutine cr_TextScaling = null;
+    //Container for the timer countdown coroutine.
+    private Coroutine cr_timerRunning = null;
+
+    [SerializeField, Tooltip("The time the timer should start from.")] private float startFromTime = 10;
+
+	// Use this for initialization
+	void Start () {
+        StartTimer();
+	}
+	
+    /// <summary>
+    /// Starts the timer.
+    /// </summary>
+    public void StartTimer()
+    {
+        CoroutineManager.BeginCoroutine(RunTimer(startFromTime), cr_timerRunning, this);
+    }
+
+    /// <summary>
+    /// Counts the timer down while also updating the graphic and text displays.
+    /// </summary>
+    /// <param name="fromTime">The time the timer should start from.</param>
+    /// <returns></returns>
+    private IEnumerator RunTimer(float fromTime)
+    {
+        float t = fromTime;
+        int timeDisplayed = (int)fromTime + 1;
+        SetLabelText(timeDisplayed.ToString());
+
+        while(t > 0)
+        {
+            t -= Time.deltaTime;
+            timerGraphic.fillAmount = t / fromTime;
+
+            if(timeDisplayed != (int)t + 1)
+            {
+                timeDisplayed = (int)t + 1;
+                SetLabelText(timeDisplayed.ToString());
+            }
+
+            yield return null;
+        }
+
+        TimeUp.Invoke();
+    }
+
+    /// <summary>
+    /// Sets the label of the timer to reflect the time remaining. If withAnimation is true, will pulse with the change.
+    /// </summary>
+    /// <param name="text">What should the label read?</param>
+    /// <param name="withAnimation">Should the new text animate?</param>
+    public void SetLabelText(string text, bool withAnimation = true)
+    {
+        timerText.text = text;
+        if (withAnimation)
+        {
+            CoroutineManager.BeginCoroutine(CoroutineManager.ShrinkScaleFrom(timerTextTransform, new Vector3(2.0f, 2.0f, 2.0f), Vector3.one, 1.0f, cr_TextScaling), cr_TextScaling, this);
+        }
+    }
+
+    /// <summary>
+    /// Resets then stars the timer.
+    /// </summary>
+    public void RestartTimer()
+    {
+        ResetTimer((int)startFromTime);
+        StartTimer();
+    }
+
+    /// <summary>
+    /// Resets the timer to 
+    /// </summary>
+    /// <param name="displayedTime"></param>
+    public void ResetTimer(int displayedTime)
+    {
+        SetLabelText(displayedTime.ToString(), false);
+        timerGraphic.fillAmount = 1;
+    }
+
+
+
+    /****************************************************************************************************/
+    /********************************************** EVENTS **********************************************/
+    /****************************************************************************************************/
+
+    public UnityEvent TimeUp;
+
+}
