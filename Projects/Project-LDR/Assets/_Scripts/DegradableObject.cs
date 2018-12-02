b3933ef26e55172cf0b00857d62619c3c1a26370
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2018-12-02 11:13:49 -0500

    New art added + steady degredation over time.

---
+++ Projects/Project-LDR/Assets/_Scripts/DegradableObject.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DegradableObject : MonoBehaviour {
+
+    [SerializeField] private GameObject[] degradationItems;
+    [SerializeField] private Stats degradeBadedOn;
+
+    private void Start()
+    {
+        switch (degradeBadedOn)
+        {
+            case Stats.SOCIAL:
+                Player.Instance.SocialChanged.AddListener(CheckDegradation);
+                break;
+            case Stats.SLEEP:
+                Player.Instance.SleepChanged.AddListener(CheckDegradation);
+                break;
+            case Stats.SUCCESS:
+                Player.Instance.SuccessChanged.AddListener(CheckDegradation);
+                break;
+            default:
+                break;
+        }
+
+        CheckDegradation();
+    }
+
+    public void CheckDegradation()
+    {
+        float maxStat = 0;
+        float currStat = 0;
+        switch (degradeBadedOn)
+        {
+            case Stats.SOCIAL:
+                maxStat = Player.Instance.maxSocial;
+                currStat = Player.Instance.currentSocial;
+                break;
+            case Stats.SLEEP:
+                maxStat = Player.Instance.maxSleep;
+                currStat = Player.Instance.currentSleep;
+                break;
+            case Stats.SUCCESS:
+                maxStat = Player.Instance.maxSuccess;
+                currStat = Player.Instance.currentSuccess;
+                break;
+            default:
+                break;
+        }
+
+        int upperIndex = (int)Mathf.Lerp(degradationItems.Length +1, 0, currStat / maxStat);
+
+        for(int i = 0; i < degradationItems.Length; i++)
+        {
+            if(i < upperIndex)
+            {
+                degradationItems[i].SetActive(true);
+            } else
+            {
+                degradationItems[i].SetActive(false);
+            }
+        }
+    }
+}
