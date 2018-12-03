8163d7ff06f56a7ca097b9831b8ea79e207bfe50
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2018-12-02 23:18:54 -0500

    Implemented new activity graphics & fixed typo

---
+++ Projects/Project-LDR/Assets/_Scripts/DedicationImage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DedicationImage : MonoBehaviour {
+
+    [SerializeField] private DayManager.TimeOfDay targetTimeOfDay;
+    [SerializeField] private DedicationImagePool pool;
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        DayManager.Instance.DayEnd += DayManager_DayEnd;
+
+        switch (targetTimeOfDay)
+        {
+            case DayManager.TimeOfDay.Morning:
+                DayManager.Instance.MorningDedicationChanged.AddListener(UpdateLabel);
+                break;
+            case DayManager.TimeOfDay.Evening:
+                DayManager.Instance.EveningDedicationChanged.AddListener(UpdateLabel);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void DayManager_DayEnd(object sender, DayManager.DayEndArgs e)
+    {
+        image.enabled = false;
+    }
+
+    public void UpdateLabel()
+    {
+        image.enabled = true;
+        Stats s = (Stats)DayManager.Instance.DedicatedStatIndex;
+
+        image.sprite = pool.GetImage(s);
+    }
+}
---
+++ Projects/Project-LDR/Assets/_Scripts/DedicationImagePool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Image Pool", menuName = "Dedication Image Pool")]
+public class DedicationImagePool : ScriptableObject {
+
+    [SerializeField] Sprite[] images;
+
+    public Sprite GetImage(Stats s)
+    {
+        int i = (int)s;
+
+        return images[i];
+    }
+}
